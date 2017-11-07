283a8467c1a459b34ce620f20f9b750e6c6baac9
Daniel Hollcraft <daniel@hollcraft.com>
2017-11-06 19:14:05 -0500

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express() // creates an instance of an express application
+const morgan = require('morgan')
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+// app.disable('etag')
+
+app.get('/*', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+
+const port = 3000
+app.listen(port, () => console.log('listening to ' + port))
